69f7e0f71228eac01f39c5701c2253270cfceed4
Beke83 <bekebenjamin@yahoo.com>
2020-04-11 16:10:11 +0100

    Initial Commit

---
+++ Reactivities/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Reactivities
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //host used to store what is returned from  WebHost.CreateDefaultBuilder
+            var host = CreateWebHostBuilder(args).Build();
+
+            //getting datacontext service
+            using (var scope = host.Services.CreateScope() )
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger< Program >> ();
+                    logger.LogError(ex, "An error occurred during migration");
+                }
+   
+            }
+            host.Run();
+
+
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
